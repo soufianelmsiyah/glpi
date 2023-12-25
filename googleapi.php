293514425a3b0a36792db4dfc3cf58adf3103023
Soufian EL MSIYAH <soufian.elmsiyah@gmail.com>
2023-12-25 14:33:50 +0100

    glp project

---
+++ googleapi.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+define('GOOGLE_CLIENT_ID', '681059585565-g8md1o6bjcp7eqvu564vjl81dd3vljuu.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URL', 'https://localhost/glpi');
+
+?>
+
